2247c5632f666fe15e8be79c2f13671799e558e1
Samuel <samuelsimplicio@poli.ufrj.br>
2020-04-28 12:54:47 -0300

    Added text to reffer to the patient name on the email body

--- mailling_system.py
+++ mailling_system.py
@@ -11,7 +11,7 @@ def create_body(rame, result, date):
     Creates the body of the email based on the result
     """
 
-    message = f"Prezado {name}\n\nSegue em anexo o Laudo Técnico de Diagnóstico Molecular para o Vírus SARS-CoV2 de exame realizado na UFRJ no dia {date}."
+    message = f"Prezado {name},\n\nSegue em anexo o Laudo Técnico de Diagnóstico Molecular para o Vírus SARS-CoV2 de exame realizado na UFRJ no dia {date}."
 
     if result == "positivo": 
         message += "\nSolicitamos que retorne esse e-mail nos atualizando sobre o seu estado de saúde atual e que não hesite em contactar em caso de dúvidas. Torcemos por uma breve recuperação."
